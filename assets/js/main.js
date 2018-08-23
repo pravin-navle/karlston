edb51e16bad326213e2990b0ef107387afc39596
Pravin Navle <navlepravin1@gmail.com>
2018-08-23 15:10:49 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,38 @@ heroSlider.slick({
     nextArrow: '<button type="button" class="slick-next"><i class="icofont icofont-long-arrow-right"></i></button>',
 });
     
+
+
+$('.coolslider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+autoplay : true,
+autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
 /*--
 	Product Slider
 -----------------------------------*/
